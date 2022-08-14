a102b14d30fe0a1e47990b53035026d5c1f868f9
AlexanderA24 <97633879+AlexanderA24@users.noreply.github.com>
2022-08-14 20:31:49 +0100

    Update index.js

--- index.js
+++ index.js
@@ -47,8 +47,10 @@ const {ObjectId} = require("mongoose").Types;
 
 // mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
 
-mongoose.connect(process.env.CONNECTION_URI,  
-  { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.CONNECTION_URI, (err) => {
+  if (err) throw err;
+  console.log('Connected to MongoDB!!!');
+}); 
 
 mongoose.set('debug', true);
   // GET requests
