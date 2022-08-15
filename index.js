32f6742b7933ef5b78ce7b927ace8fc766378766
AlexanderA24 <97633879+AlexanderA24@users.noreply.github.com>
2022-08-15 16:49:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -21,19 +21,22 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const cors = require('cors');
+app.use(cors());
 
-let allowedOrigins = ['http://localhost:8080', '82.11.35.21/32'];
+// const cors = require('cors');
 
-app.use(cors({
-  origin: (origin, callback) => {
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
-      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-      return callback(new Error(message ), false);
-    }
-    return callback(null, true);
-  }
-}));
+// let allowedOrigins = ['http://localhost:8080', '82.11.35.21/32'];
+
+// app.use(cors({
+//   origin: (origin, callback) => {
+//     if(!origin) return callback(null, true);
+//     if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+//       let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+//       return callback(new Error(message ), false);
+//     }
+//     return callback(null, true);
+//   }
+// }));
 
 /* rest of code goes here*/
 
