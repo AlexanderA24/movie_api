73dc3fca8fff161a5fcd86bea08ed745761a0f88
AlexanderA24 <97633879+AlexanderA24@users.noreply.github.com>
2022-10-07 16:36:19 +0100

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const cors = require('cors');
-app.use(cors());
+app.use(cors({ origin: '*' }));
 
 // const cors = require('cors');
 
